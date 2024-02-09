d61afe989e72df6ba809405105a915bcd4ede4d9
likhithbr1 <148860387+likhithbr1@users.noreply.github.com>
2024-02-09 21:29:22 +0530

    Create player.cs

---
+++ player.cs
@@ -0,0 +1,62 @@
+ using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+public GameObject panel;
+
+    private float dirX =0f;
+   [SerializeField] private float moveSpeed =7f;
+    [SerializeField]private float jumpForce=14f;
+
+    // Start is called before the first frame update
+ 
+
+     private void Start()
+    {
+       rb=GetComponent<Rigidbody2D>(); 
+       sprite=GetComponent<SpriteRenderer>();
+      anim=GetComponent<Animator>();
+      panel.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX=Input.GetAxis("Horizontal");
+        rb.velocity=new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if(Input.GetKey("space"))
+        {
+            rb.velocity=new Vector2(rb.velocity.x,jumpForce);
+        }
+        UpdateAnimationUpdate();
+    }
+    private void UpdateAnimationUpdate()
+    {
+          if(dirX > 0f)
+        {
+            anim.SetBool("running",true);
+            sprite.flipX=false;
+
+
+        }
+        else if(dirX < 0f)
+        {
+            anim.SetBool("running",true);
+            sprite.flipX=true;
+        }
+        else
+        {
+        anim.SetBool("running",false);
+        }
+        
+
+    }
+}
